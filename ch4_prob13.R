91b0f6f3cf20067ad22e7c2020b34a5fbaa724d2
Grecia RO <56781403+greciaro@users.noreply.github.com>
2021-11-18 15:12:04 -0800

    Add files via upload

---
+++ ch4_prob13.R
@@ -0,0 +1,4 @@
+#__________________________Ch4 Prob13________________________________
+#____Using Logistic Regression, LDA and KNN to predict wheter a suburb of Boston data set has a crime rate above or below the median______________
+
+
