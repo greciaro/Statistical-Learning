9b64b063b1ae3391661dc944a9bb4901e7503c5b
Grecia RO <56781403+greciaro@users.noreply.github.com>
2021-11-22 21:29:48 -0800

    Add files via upload

--- ch4_prob13.R
+++ ch4_prob13.R
@@ -17,7 +17,7 @@ set.seed(0)
 #Save number of observations in the Boston dataset
 n = dim(Boston)[1]
 
-#Create a vector of "n" size of values 0 or 1 depending if each observation of coumn "crim" is above or bellow the median
+#Create a vector of "n" size with values depending if each observation of coumn "crim" is above (1) or bellow(0) the median
 Boston$crim_0_or_1 = integer(n)
 Boston$crim_0_or_1[ Boston$crim >= median( Boston$crim ) ] = 1
 
@@ -25,9 +25,23 @@ Boston$crim_0_or_1[ Boston$crim >= median( Boston$crim ) ] = 1
 Boston$crim = NULL
 
 #Find the columns that are most correlated with the behavior of crim being above or below the median (crim_0_or_1)
-list_best_corr = rev(sort( cor(Boston)[,'crim_0_or_1'] ))
+list_best_corr = rev(sort( abs(cor(Boston)[,'crim_0_or_1'] )))
 print(list_best_corr)
+three_best_corr = names(list_best_corr)[2:4]
+print(three_best_corr)
 
+#Divide data set, between the trainning and testing data sets
+splitting_portion = 3*n/4
+inds_train = sample(1:n,splitting_portion)
+inds_test = (1:n)[-inds_train]
+Boston_train = Boston[inds_train,]
+Boston_test = Boston[inds_test,]
 
+#Fit models and calculate test errors
 
-
+#Logistic Regression
+lr_model = glm( crim_0_or_1 ~ nox + rad + dis, data=Boston_train, family=binomial)
+prediction = predict( lr_model, newdata=Boston_test, type="response" )
+binary_prediction = rep( 0, length(prediction))
+binary_prediction[ prediction > 0.5 ] = 1
+CM = table( predicted=binary_prediction, truth=Boston_test$crim_0_or_1 )
