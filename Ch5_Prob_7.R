10f0146adceb6b76c7d570fe3711ffcdb52dc200
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-01-28 10:06:17 -0800

    Add files via upload

--- Ch5_Prob_7.R
+++ Ch5_Prob_7.R
@@ -12,8 +12,9 @@ m_0 = glm( Direction ~ Lag1 + Lag2, data=Weekly, family="binomial" )
 m_loocv = glm( Direction ~ Lag1 + Lag2, data=Weekly[-1,], family="binomial" )
 
 #Part (c):
-print( sprintf( "Prediction on first sample is %d (1=>Up; 0=>Down)", predict( m_loocv, newdata=Weekly[1,] ) > 0 ) )
-print( sprintf( "True direction of the frist sample is %s", Weekly[1,]$Direction ) )
+print( sprintf( "c) Prediction using glm on first sample is %d (1=>Up; 0=>Down)", predict( m_0, newdata=Weekly[1,] ) > 0 ) )
+print( sprintf( "c) Prediction using LOOCVirst sample is %d (1=>Up; 0=>Down)", predict( m_loocv, newdata=Weekly[1,] ) > 0 ) )
+print( sprintf( "c) True direction of the frist sample is %s", Weekly[1,]$Direction ) )
 
 #Part (d):
 n = dim(Weekly)[1]
@@ -21,12 +22,15 @@ number_of_errors = 0
 for( ii in 1:n ){
   m_loocv = glm( Direction ~ Lag1 + Lag2, data=Weekly[-ii,], family="binomial" )
 
-  error_1 = ( predict( m_loocv, newdata=Weekly[ii,] ) > 0 ) & ( Weekly[ii,]$Direction == "Down" )
-  error_2 = ( predict( m_loocv, newdata=Weekly[ii,] ) < 0 ) & ( Weekly[ii,]$Direction == "Up" )
+  error_1 = ( predict( m_loocv, newdata=Weekly[ii,] ) > 0.5 ) & ( Weekly[ii,]$Direction == "Down" )
+  error_2 = ( predict( m_loocv, newdata=Weekly[ii,] ) < 0.5 ) & ( Weekly[ii,]$Direction == "Up" )
   if( error_1 | error_2 ){
     number_of_errors = number_of_errors + 1 
   }
+  print("")
 }
 
+
 #part(e)
-print( sprintf( "LOOCV test error rate= %10.6f", number_of_errors/n ) )
+
+print( sprintf( "e) LOOCV test error rate= %10.6f", number_of_errors/n ) )
