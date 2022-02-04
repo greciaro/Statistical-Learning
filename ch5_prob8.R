f6d6ba104528368c7dcad1c7ccd19227c21b111b
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-02-03 22:11:26 -0800

    Add files via upload

--- ch5_prob8.R
+++ ch5_prob8.R
@@ -1,33 +1,40 @@
-if( ! require("ISLR") ){ install.packages("ISLR") }
-library(boot)
+install.packages("ISLR")
+
 
 # Part (a):
 #
 set.seed(1)
-
 x = rnorm(100)
 y = x - 2 * x^2 + rnorm(100)
-
-#postscript("../../WriteUp/Graphics/Chapter5/prob_8_scatter_plot.eps", onefile=FALSE, horizontal=FALSE)
 plot( x, y )
-#dev.off()
 
 DF = data.frame( y=y, x=x )
 
+
+
 # Do cross-validation on each model:
-# 
-m_i = glm( y ~ x, data=DF )
-cv.err = cv.glm( DF, m_i )
+library(ISLR)
+Data = data.frame(x, y)
+set.seed(1)
+# i.
+glm.fit = glm(y ~ x)
+cv.glm(Data, glm.fit)$delta
+
+
+
+
+model_1 = glm( y ~ x, data=DF )
+cv.err = cv.glm( DF, model_1 )
 print( sprintf( "Model (i): cv output= %10.6f", cv.err$delta[1] ) )
 
-m_ii = glm( y ~ x + I(x^2), data=DF )
-cv.err = cv.glm( DF, m_ii )
+model_2 = glm( y ~ x + I(x^2), data=DF )
+cv.err = cv.glm( DF, model_2 )
 print( sprintf( "Model (ii): cv output= %10.6f", cv.err$delta[1] ) )
 
-m_iii = glm( y ~ x + I(x^2) + I(x^3), data=DF )
-cv.err = cv.glm( DF, m_iii )
+model_3 = glm( y ~ x + I(x^2) + I(x^3), data=DF )
+cv.err = cv.glm( DF, model_3 )
 print( sprintf( "Model (iii): cv output= %10.6f", cv.err$delta[1] ) )
 
-m_iv = glm( y ~ x + I(x^2) + I(x^3) + I(x^4), data=DF )
-cv.err = cv.glm( DF, m_iv )
+model_4 = glm( y ~ x + I(x^2) + I(x^3) + I(x^4), data=DF )
+cv.err = cv.glm( DF, model_4 )
 print( sprintf( "Model (iv): cv output= %10.6f", cv.err$delta[1] ) )
